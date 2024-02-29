3346cd5c0f3847c71996f0b8b69a506e59edfc3b
Neo40 <tron36.cc@gmail.com>
2024-02-29 19:05:46 -0300

    Adição de classe ProdutoService

---
+++ api/src/main/java/com/avanade/dio/api/service/ProdutoService.java
@@ -0,0 +1,20 @@
+package com.avanade.dio.api.service;
+
+import com.avanade.dio.api.models.Produto;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class ProdutoService {
+    public List<Produto> findAll() {
+        List<Produto> listaProduto = new ArrayList<Produto>();
+        Produto p1 = new Produto(1, "descricao 1", new Date(), "123456", true);
+        Produto p2 = new Produto(2, "descricao 2", new Date(), "12345678", true);
+        listaProduto.add(p1);
+        listaProduto.add(p2);
+        return listaProduto;
+    }
+}
