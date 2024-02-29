d226b06ca4e4769f54ccec1ec4565401ccc24ba7
Neo40 <tron36.cc@gmail.com>
2024-02-29 19:05:46 -0300

    Adição de produtoService em Controler

--- api/src/main/java/com/avanade/dio/api/controller/ProdutoController.java
+++ api/src/main/java/com/avanade/dio/api/controller/ProdutoController.java
@@ -1,14 +1,22 @@
 package com.avanade.dio.api.controller;
 
+import com.avanade.dio.api.models.Produto;
+import com.avanade.dio.api.service.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("api/v1/produtos")
 public class ProdutoController {
+    @Autowired
+    private ProdutoService produtoService;
+
     @GetMapping
-    public String listar() {
-        return "Lista de Produtos";
+    public List<Produto> listar() {
+        return produtoService.findAll();
     }
 }
