7f65c3f7ecfcb53e8b2e8ba37c498a79514daf60
Neo40 <tron36.cc@gmail.com>
2024-03-05 22:51:04 -0300

    Adição de PostMapping

--- api/src/main/java/com/avanade/dio/api/controller/ProdutoController.java
+++ api/src/main/java/com/avanade/dio/api/controller/ProdutoController.java
@@ -3,9 +3,8 @@ package com.avanade.dio.api.controller;
 import com.avanade.dio.api.models.Produto;
 import com.avanade.dio.api.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,5 +17,11 @@ public class ProdutoController {
     @GetMapping
     public List<Produto> listar() {
         return produtoService.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void inserir(@RequestBody Produto produto) {
+
     }
 }
